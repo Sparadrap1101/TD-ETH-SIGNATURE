90467744134db4510e2ff9b6da82b6845441e0db
Sparadrap1101 <alexis111@orange.fr>
2022-11-27 17:35:29 +0100

    Interface contracts.

---
+++ contracts/IExerciceSolution.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.2;
+
+interface IExerciceSolution {
+    function ERC721Address() external returns (address);
+
+    function mintATokenForMe() external returns (uint256);
+
+    function mintATokenForMeWithASignature(
+        bytes calldata _signature
+    ) external returns (uint256);
+
+    function getAddressFromSignature(
+        bytes32 _hash,
+        bytes calldata _signature
+    ) external returns (address);
+
+    function signerIsWhitelisted(
+        bytes32 _hash,
+        bytes calldata _signature
+    ) external returns (bool);
+
+    function whitelist(address _signer) external returns (bool);
+}
